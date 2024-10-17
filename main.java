6b1b61a811561b905e51cec55cbc38bdb3614252
Gajula Bhavya <gajulabhavya6903@gmail.com>
2024-10-17 15:50:38 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println(args[0]);
+        } else {
+            System.out.println("No argument given.");
+        }
+    }
+
+
+}
